11055af3a2490f77d83bc55fbef16812879dcef4
TYC-dev <1096285922@qq.com>
2018-07-16 00:17:18 +0800

    update 7.15(1)
    add button

---
+++ src/components/Cbutton/Cbutton.vue
@@ -0,0 +1,97 @@
+<template>
+    <button class="c-btn"
+      :class="{
+          'c-btn-default': type === 'default',
+          'c-btn-normal': type === 'normal',
+          'c-btn-warn': type === 'warn',
+          'c-btn-disabled': disabled
+      }"
+      @click="onClick">
+        <slot>
+            <span>{{text}}</span>
+        </slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'cbutton',
+    props: {
+        text: String,
+        type: {
+            default: 'default'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        link: String
+    },
+    methods: {
+        onClick() {
+            
+        }
+    }
+}
+</script>
+
+<style lang="less">
+    .c-btn{
+        width: 100%;
+        border: none;
+        outline: 0;
+    }
+    .c-btn{
+        position: relative;
+        display: block;
+        padding: 0 14px;
+        margin-top: 10px;
+        border-radius: 6px;
+        box-sizing: border-box;
+        &::after{
+            content: '';
+            position: absolute;
+            width: 200%;
+            height: 200%;
+            top: 0;
+            left: 0;
+            border: 1px solid rgba(0, 0, 0, 0.4);
+            transform: scale(0.5);
+            transform-origin: 0 0;
+            border-radius: 12px;
+        }
+    }
+    .c-btn{
+        font-size: 18px;
+        line-height: 2.5;
+        font-weight: normal;
+        color: #fff;
+    }
+    .c-btn-default{
+        background-color: #45adca;
+    }
+    .c-btn-normal{
+        background-color: #f8f8f8;
+        color: #000;
+    }
+    .c-btn-warn{
+        background-color: #E64340;
+    }
+    .c-btn-disabled{
+        background-color: #b7b7b7;
+        background-image: linear-gradient(45deg, 
+							rgba(255,255,255,.7) 25%,
+							transparent 25%,
+							transparent 50%,
+							rgba(255,255,255,.7) 50%,
+							rgba(255,255,255,.7) 75%,
+                            transparent 75%);
+        background-size: 45px 45px;
+        color: #fff;
+    }
+    .c-btn-gradient{
+                          
+    }
+</style>
+
+
---
+++ src/components/ShowCbutton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="show-button-des">
+        <cbutton>默认</cbutton>
+        <cbutton type="normal">一般</cbutton>
+        <cbutton type="warn">警告</cbutton>
+        <cbutton :disabled="true">禁用</cbutton>
+        <!-- <cbutton link="/">路由跳转</cbutton> -->
+        <!-- <cbutton link="https://www.baidu.com">外部链接</cbutton> -->
+    </div>
+</template>
+
+<script>
+    import Cbutton from './Cbutton/Cbutton'
+
+    export default{
+        name: 'showCbutton',
+        components: {
+            Cbutton
+        },
+        methods: {
+            btnClick() {
+                console.log('click')
+            }
+        }
+    }
+</script>
+
+<style>
+    .show-button-des{
+        margin: 10px;
+    }
+</style>
