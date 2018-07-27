7b8177d0f2b8ba623f12c7baaa551257b5c868f0
TYC-dev <1096285922@qq.com>
2018-07-27 17:32:21 +0800

    update 7.27(1)
    redesigned switch
    fix switch

--- src/components/Cswitch/CSwitch.vue
+++ src/components/Cswitch/CSwitch.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="c-switch"
-      :class="{'c-switch-on': currentShow}"
-      @click="onClick">
+    @click="onClick">
+        <div class="c-switch-wrap"
+        :class="{'c-switch-on': currentShow}"></div>
         <div class="c-switch-bar"
-          :class="{'c-switch-bar-on': currentShow}"></div>
+        :class="{'c-switch-bar-on': currentShow}"></div>
     </div>
 </template>
 
@@ -23,6 +24,11 @@
             return{
                 currentShow: this.value ? this.value : false
             }
+        },
+        watch: {
+            value( val ) {
+                this.currentShow = val
+            }
         }
     }
 </script>
@@ -32,29 +38,36 @@
         border-radius: 4px;
     }
     .c-switch{
+        display: flex;
+        align-items: center;
         position: relative;
-        height: 25px;
+        height: 30px;
         width: 55px;
-        background-color: #d4d4d4;
-        transition: background-color 0.3s;
         .borderRadius;
+        &-wrap{
+            height: 16px;
+            width: 100%;
+            background-color: #d4d4d4;
+            transition: background-color 0.3s;
+            border-radius: 16px;
+        }
         &-on{
             background-color: #45adca;
         }
         &-bar{
             position: absolute;
             left: 0;
-            top: -3px;
-            width: 20px;
-            height: 31px;
+            top: 0;
+            width: 30px;
+            height: 30px;
             background-color: #fff;
             border: 1px solid #c3c3c3;
             box-sizing: border-box;
             transition: left 0.3s;
-            .borderRadius;
+            border-radius: 50%;
         }
         &-bar-on{
-            left: 35px;
+            left: 25px;
             border-color: #45adca;
         }
     }
