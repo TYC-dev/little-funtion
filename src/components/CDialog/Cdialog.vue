a3c4636636fd411604a86bd50d29ebedd014e82e
TYC-dev <1096285922@qq.com>
2018-07-28 02:21:53 +0800

    upadte 7.28(2)
    add cdialog

---
+++ src/components/CDialog/Cdialog.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="c-dialog-wrap">
+        <div class="c-dialog"
+        v-show="value">
+            <div class="c-dialog-inner">
+                <div class="c-dialog-close"
+                v-if="!onMaskHide"
+                @click="hide"></div>
+                <slot></slot>  
+            </div>
+        </div>
+        <div class="c-dialog-mask"
+        v-show="value"
+        @click="hideOnMask"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'cdialog',
+        props: {
+            value: Boolean,
+            onMaskHide: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            hide: function() {
+                this.$emit('input',false)
+            },
+            hideOnMask: function() {
+                if(!this.onMaskHide) return
+                this.$emit('input',false)
+            }
+        },
+        data() {
+            return {
+                show: this.value
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+.c-dialog-wrap{
+    .c-dialog{
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+    .c-dialog{
+        z-index: 1000;
+        display: table;
+        width: 80%;
+        min-height: 1px;
+        margin: auto;
+        background-color: #fff;
+        border-radius: 15px;
+        &-inner{
+            position: relative;
+            .c-dialog-close{
+                position: absolute;
+                right: 0;
+                top: -45px;
+                width: 25px;
+                height: 25px;
+                border: 2px solid #fff;
+                border-radius: 50%;
+                &::before{
+                    content: '';
+                    position: absolute;
+                    top: 11px;
+                    left: 5px;
+                    width: 15px;
+                    border-top: 2px solid #fff;
+                    transform: rotate(45deg);
+                }
+                &::after{
+                    content: '';
+                    position: absolute;
+                    top: 11px;
+                    left: 5px;
+                    width: 15px;
+                    border-top: 2px solid #fff;
+                    transform: rotate(315deg);
+                }
+            }
+        }
+    }
+    .c-dialog-mask{
+        z-index: 999;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: #000;
+        opacity: 0.5;
+    }
+}
+    
+
+</style>
---
+++ src/components/ShowDialog.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="show-dialog">
+        <c-group>
+            <cell title="默认样式">
+                <cswitch v-model="dialogShow"></cswitch>
+            </cell>
+            <cell title="点击遮罩关闭">
+                <cswitch v-model="dialogBlurShow"></cswitch>
+            </cell>
+        </c-group>
+
+        <cdialog v-model="dialogShow">
+            <div class="dialog-box">
+                <div class="img-box">
+                    <img src="../assets/logo.png" alt="">
+                </div>
+            </div>
+        </cdialog>
+        <cdialog v-model="dialogBlurShow" onMaskHide>
+            <div class="dialog-box">
+                <div class="img-box">
+                    <img src="../assets/logo.png" alt="">
+                </div>
+            </div>
+        </cdialog>
+    </div>
+</template>
+
+<script>
+import Cdialog from './CDialog/Cdialog'
+import CGroup from './Cgroup/CGroup'
+import Cell from './Cell/Cell'
+import Cswitch from './Cswitch/CSwitch'
+export default {
+    name: 'showdialog',
+    components: {
+        Cdialog,
+        CGroup,
+        Cell,
+        Cswitch
+    },
+    data() {
+        return {
+            dialogShow: false,
+            dialogBlurShow: false
+        }
+    }
+}
+</script>
+
+<style lang="less">
+.dialog-box{
+    padding: 10px;
+    .img-box{
+        img{
+            width: 90%;
+        }
+    }
+}
+</style>
