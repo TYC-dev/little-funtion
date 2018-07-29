0525a427a853f74e5669bbb01a28d8037ca8fef2
TYC-dev <1096285922@qq.com>
2018-07-29 23:58:15 +0800

    update 7.29(4)
    changed group demo

--- src/components/ShowGroup.vue
+++ src/components/ShowGroup.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="show-group">
         <c-group>
-            <cell title="title" isLink></cell>
+            <cell title="中文标题" isLink></cell>
             <cell title="title">right-text</cell>
             <cell title="disabled" disabled>true</cell>
             <cell title="route link" isLink link="/tips">link</cell>
@@ -13,6 +13,9 @@
                     <div class="small-box">1234</div>
                 </div>
             </cell>
+            <cell title="title">
+                <cswitch v-model="switch1"></cswitch>
+            </cell>
         </c-group>
     </div>
     
@@ -21,11 +24,18 @@
 <script>
 import CGroup from './Cgroup/CGroup'
 import Cell from './Cell/Cell'
+import Cswitch from './Cswitch/CSwitch'
 export default {
     name: 'showgroup',
     components: {
         CGroup,
-        Cell
+        Cell,
+        Cswitch
+    },
+    data() {
+        return {
+            switch1: false
+        }
     }
 }
 </script>
@@ -38,8 +48,9 @@ export default {
         position: relative;
         float: left;
         height: 32px;
-        padding: 10px 20px;
-        font-size: 20px;
+        padding: 0 20px;
+        font-size: 16px;
+        line-height: 32px;
         background-color: #c9c9c9;
         color: #fff;
         border-radius: 32px;
